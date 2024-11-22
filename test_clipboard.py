c141d79e5aaba207b6122164b1ef2788a7dbbf70
alexsevas <mralexrelax@gmail.com>
2024-11-22 21:41:46 +0300

    Add test_clipboard

---
+++ test_clipboard.py
@@ -0,0 +1,13 @@
+# conda activate jarvis2
+
+import pyperclip
+
+def get_clipboard_text():
+    clipboard_content = pyperclip.paste()
+    if isinstance(clipboard_content, str):
+        return clipboard_content
+    else:
+        print('No clipboard text to copy')
+        return None
+
+print(get_clipboard_text())
